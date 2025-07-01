10cc112931249a4849523adda1ec7aeb67dea5e5
MominovAzizDev <azizmominov0023@email.com>
2025-07-01 14:49:37 +0500

    add imagepicker

--- lib/chat/chatpages.dart
+++ lib/chat/chatpages.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class Chatpages extends StatefulWidget {
@@ -11,27 +14,28 @@ class Chatpages extends StatefulWidget {
 class _ChatpagesState extends State<Chatpages> {
   final TextEditingController _controller = TextEditingController();
   late WebSocketChannel channel;
-  final List<String> messages = [];
+  final ImagePicker picker = ImagePicker();
+  final List<Map<String, dynamic>> messages = [];
 
   @override
   void initState() {
     super.initState();
-    final channel = WebSocketChannel.connect(
-      Uri.parse("ws://192.168.1.10:8080"),
-    );
+    channel = WebSocketChannel.connect(Uri.parse("ws://192.168.1.10:8080"));
 
     channel.stream.listen((message) {
+      final decoded = jsonDecode(message);
       setState(() {
-        messages.add("$message");
+        messages.add({"type": decoded["type"], "data": decoded["data"]});
       });
     });
   }
 
   void _sent_messages() {
     if (_controller.text.isNotEmpty) {
-      channel.sink.add(_controller.text);
+      final String text = _controller.text;
+      channel.sink.add(jsonEncode({"type": "text", "data": text}));
       setState(() {
-        messages.add("siz ${_controller.text}");
+        messages.add({"type": "text", "data": text});
       });
       _controller.clear();
     }
@@ -44,6 +48,48 @@ class _ChatpagesState extends State<Chatpages> {
     super.dispose();
   }
 
+  void imagepickerchoose() {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              leading: Icon(Icons.camera_alt),
+              title: Text("Camera"),
+              onTap: () {
+                Navigator.pop(context);
+                _pickImage(ImageSource.camera);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.image_rounded),
+              title: Text("Galareya"),
+              onTap: () {
+                Navigator.pop(context);
+                _pickImage(ImageSource.gallery);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _pickImage(ImageSource source) async {
+    final PickedFile = await picker.pickImage(source: source, imageQuality: 50);
+    if (PickedFile != null) {
+      final bytes = await PickedFile.readAsBytes();
+      final base64Image = base64Encode(bytes);
+
+      channel.sink.add(jsonEncode({"type": "image", "data": base64Image}));
+      setState(() {
+        messages.add({"type": "image", "data": base64Image});
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,7 +99,17 @@ class _ChatpagesState extends State<Chatpages> {
             child: ListView.builder(
               itemCount: messages.length,
               itemBuilder: (BuildContext context, int index) {
-                return ListTile(title: Text(messages[index]));
+                final msg = messages[index];
+                if (msg["type"] == "text") {
+                  return ListTile(title: Text(msg["data"]));
+                } else if (msg["type"] == "image") {
+                  return Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Image.memory(base64Decode(msg["data"])),
+                  );
+                } else {
+                  return SizedBox.shrink();
+                }
               },
             ),
           ),
@@ -61,11 +117,14 @@ class _ChatpagesState extends State<Chatpages> {
             padding: EdgeInsets.only(left: 20, right: 20),
             child: Row(
               children: [
-               Expanded(child:  TextField(
-                  controller: _controller,
-                  decoration: InputDecoration(hintText: "Enter somthing.."),
-                ),),
-                IconButton(onPressed:_sent_messages, icon: Icon(Icons.send)),
+                IconButton(onPressed:imagepickerchoose, icon: Icon(Icons.add)),
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(hintText: "Enter somthing.."),
+                  ),
+                ),
+                IconButton(onPressed: _sent_messages, icon: Icon(Icons.send)),
               ],
             ),
           ),
